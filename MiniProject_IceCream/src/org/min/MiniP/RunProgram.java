b1f18ff46c9f114b8cde0ecf2df9e287b8d85a3c
Guksu <87972252+Guksu@users.noreply.github.com>
2021-09-14 16:08:45 +0900

    Day-2 완성본

--- MiniProject_IceCream/src/org/min/MiniP/RunProgram.java
+++ MiniProject_IceCream/src/org/min/MiniP/RunProgram.java
@@ -1,26 +1,28 @@
 package org.min.MiniP;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class RunProgram {
-
+public class RunProgram{
+	
+	DecimalFormat formatter = new  DecimalFormat("###,###.##");
 	List<SetProgram> monthSales = new ArrayList<>() ;
-	int[] month = {1,2,3,4,5,6,7,8,9,10,11,12};
 	Scanner sc;
 	
 	public RunProgram() {
 		sc = new Scanner(System.in);
+		for(int i =0; i<12; i++) {
+			monthSales.add(i,new SetProgram(0,0));
+		}
 	}
-
-	
 	
 	public void menu() {
 		System.out.println("1. 월별 매출 확인");
-		System.out.println("2. 판매량 입력");
-		System.out.println("3. 판매량 수정");
-		System.out.println("4. 매출비교");
+		System.out.println("2. 판매량 입력 혹은 수정");
+		System.out.println("3. 매출비교");
+		System.out.println("4. 가격 혹은 원가 변경");
 		System.out.println("0. 프로그램 종료");
 	}
 	
@@ -35,82 +37,148 @@ public class RunProgram {
 		double totalIncome =0;
 		
 		if(select1 == 1) {
-			for(int i=0; i<month.length; i++) {
+			for(int i=0; i<monthSales.size(); i++) {
 				mintSalesVolume += monthSales.get(i).getMintSalesVolume();
 				shootingSalesVolume += monthSales.get(i).getShootingSalesVolume();
 				totalSales += monthSales.get(i).getSales();
 				totalIncome += monthSales.get(i).getIncome();
+				System.out.println((i+1)+"월 민트 판매량: "+monthSales.get(i).getMintSalesVolume()+"개"+" , 슈팅스타 판매량: "+monthSales.get(i).getShootingSalesVolume()+"개");
 			}
+			System.out.println("--------------------------");
 			System.out.println("연간 민트 판매량: "+ mintSalesVolume+"개"+"\n연간 슈팅스타 판매량: "+shootingSalesVolume+"개"+
-					"\n연간 총 매출: "+totalSales+"원"+"\n연간 총 순이익: "+ totalIncome+"원");
+					"\n연간 총 매출: "+formatter.format(totalSales)+"원"+"\n연간 총 순이익: "+ formatter.format(totalIncome)+"원");
 		} 
 		else if(select1 == 2) {
 			System.out.print("확인할 월을 입력하세요 >>");
-			int select2 = sc.nextInt();
-			for(int i=0; i<month.length; i++) {
-				if(select2 == i) {
-					mintSalesVolume += monthSales.get(i).getMintSalesVolume();
-					shootingSalesVolume += monthSales.get(i).getShootingSalesVolume();
-					totalSales += monthSales.get(i).getSales();
-					totalIncome += monthSales.get(i).getIncome();
-				}
+			int select2 = (sc.nextInt());
+			if( select2<=0 ||  select2>12) {
+				System.out.println("다시 입력하세요");
+				showSales();
+			}else {
+				mintSalesVolume = monthSales.get(select2-1).getMintSalesVolume();
+				shootingSalesVolume = monthSales.get(select2-1).getShootingSalesVolume();
+				totalSales = monthSales.get(select2-1).getSales();
+				totalIncome = monthSales.get(select2-1).getIncome();
+
+				System.out.println("["+select2+"]월"+ "\n민트 판매량: "+ mintSalesVolume+"개"+"\n슈팅스타 판매량: "+shootingSalesVolume+"개"+
+						"\n매출액: "+formatter.format(totalSales)+"원"+"\n순이익: "+ formatter.format(totalIncome)+"원");
 			}
-			System.out.println("["+select2+"]월"+ "\n민트 판매량: "+ mintSalesVolume+"개"+"\n슈팅스타 판매량: "+shootingSalesVolume+"개"+
-					"\n매출액: "+totalSales+"원"+"\n순이익: "+ totalIncome+"원");
 		} 
+		else {
+			System.out.println("다시 입력하세요");
+			showSales();
+		}
 	}
-	
 	
 	public void inputSales() {
 		System.out.print("입력할 월을 입력하세요 >>");
 		int inputSelect = sc.nextInt();
-		System.out.print("민트아이스크림 판매량을 입력하세요 >>");
-		int mintSalesVolume = sc.nextInt();
-		System.out.print("슈팅스타 판매량을 입력하세요 >>");
-		int shootingSalesVolume = sc.nextInt();
-		
-		for(int i = 0; i<month.length; i++) {
-			if(inputSelect == month[i]) {
-				monthSales.add(i,new SetProgram(mintSalesVolume, shootingSalesVolume));
-			}
+		if(inputSelect<=0 || inputSelect>12) {
+			System.out.println("다시 입력하세요");
+			inputSales();
+		}else {
+			System.out.print("민트아이스크림 판매량을 입력하세요 >>");
+			int mintSalesVolume = sc.nextInt();
+			System.out.print("슈팅스타 판매량을 입력하세요 >>");
+			int shootingSalesVolume = sc.nextInt();
+
+			monthSales.set(inputSelect-1,new SetProgram(mintSalesVolume, shootingSalesVolume));
 		}
 	}
 
-	public void modifySales() {
-		System.out.print("수정할 월을 입력하세요>>");
-		int modifySelect = sc.nextInt();
-		System.out.print("수정할 민트아이스크림 판매량을 입력하세요 >>");
-		int modifyMintSalesVolume = sc.nextInt();
-		System.out.print("수정할 슈팅스타 판매량을 입력하세요 >>");
-		int modifyShootingSalesVolume = sc.nextInt();
-
-		for(int i = 0; i<month.length; i++) {
-			if(modifySelect == month[i]) {
-				monthSales.set(i,new SetProgram(modifyMintSalesVolume, modifyShootingSalesVolume));
-			} 
-		}
-	}
-	
 	public void compareSales() {
 		System.out.print("비교하고 싶은 월 두 개를 입력하세요 >>");
 		int selectMonth1 = sc.nextInt();
-		double selectMonth1Sales = 0;
 		sc.nextLine();
 		int selectMonth2 = sc.nextInt();
+		double selectMonth1Sales = 0;
 		double selectMonth2Sales = 0;
-		
-		for(int i =0; i<month.length; i++) {
-			selectMonth1Sales = monthSales.get(selectMonth1).getSales();
-			selectMonth2Sales = monthSales.get(selectMonth2).getSales();
+		if(selectMonth1<=0 || selectMonth1>12 || selectMonth2<=0 || selectMonth1>12 ||selectMonth1 == selectMonth2) {
+			System.out.println("다시 입력하세요");
+			compareSales();
+		}else {
+			for(int i =0; i<monthSales.size(); i++) {
+				selectMonth1Sales = monthSales.get(selectMonth1-1).getSales();
+				selectMonth2Sales = monthSales.get(selectMonth2-1).getSales();
+			}
+
+			if(selectMonth1Sales > selectMonth2Sales) {
+				System.out.println(selectMonth1+"월은 "+selectMonth2+"월보다 매출액이 ["+formatter.format(selectMonth1Sales-selectMonth2Sales)+"]원 더 많습니다.");
+			} else if(selectMonth1Sales < selectMonth2Sales) {
+				System.out.println(selectMonth2+"월은 "+selectMonth1+"월보다 매출액이 ["+formatter.format(selectMonth2Sales-selectMonth1Sales)+"]원 더 많습니다.");
+			} else if(selectMonth1Sales == selectMonth2Sales) {
+				System.out.println("두 월은 매출액이 ["+formatter.format(selectMonth1Sales)+"]원으로 같습니다");
+			}
 		}
-		
-		if(selectMonth1Sales > selectMonth2Sales) {
-			System.out.println(selectMonth1+"월은 "+selectMonth2+"월보다 매출액이 ["+(selectMonth1Sales-selectMonth2Sales)+"]원 더 많습니다.");
-		} else if(selectMonth1Sales < selectMonth2Sales) {
-			System.out.println(selectMonth2+"월은 "+selectMonth1+"월보다 매출액이 ["+(selectMonth2Sales-selectMonth1Sales)+"]원 더 많습니다.");
-		} else if(selectMonth1Sales == selectMonth2Sales) {
-			System.out.println("두 월은 매출액이 ["+selectMonth1Sales+"]원으로 같습니다");
+	}
+	
+	public void changeExpense() {
+		System.out.println("1. 가격변경");
+		System.out.println("2. 원가변경");
+		int select1 = sc.nextInt();
+		if(select1 ==1 ) {
+			System.out.println("현재 민트 판매가는 : "+monthSales.get(0).getMintPrice()+"원 , 슈팅스타 판매가는 : "+monthSales.get(0).getShootingStarPrice()+"원 입니다.");
+			System.out.println("변경하실 판매가를 선택하세요");
+			System.out.println("[1. 민트 판매가 변경\t 2. 슈팅스타 판매가 변경\t3.모두 변경]");
+			int select2 = sc.nextInt();
+			if(select2 == 1 ) {
+				System.out.println("변경할 가격을 입력하세요");
+				int changeMintPrice = sc.nextInt();
+				for(int i=0; i<monthSales.size(); i++) {
+					monthSales.get(i).setMintPrice(changeMintPrice);
+				}
+			} else if(select2 ==2) {
+				System.out.println("변경할 가격을 입력하세요");
+				int changeShootingPrice = sc.nextInt();
+				for(int i=0; i<monthSales.size(); i++) {
+					monthSales.get(i).setShootingStarPrice(changeShootingPrice);
+				}
+			} else if(select2 == 3) {
+				System.out.println("변경할 민트와 슈팅스타의 가격을 순서대로 입력하세요");
+				int changeMintPrice = sc.nextInt();
+				int changeShootingPrice = sc.nextInt();
+				for(int i=0; i<monthSales.size(); i++) {
+					monthSales.get(i).setMintPrice(changeMintPrice);
+					monthSales.get(i).setShootingStarPrice(changeShootingPrice);
+				}
+			}
+		} 
+		else if(select1 ==2) {
+			System.out.println("현재 민트 원가는 : "+monthSales.get(0).getMintExpense()+"원 , 슈팅스타 원가는 : "+monthSales.get(0).getShootingStarExpense()+"원 입니다.");
+			System.out.println("변경하실 원가를 선택하세요");
+			System.out.println("[1. 민트 원가 변경\t 2. 슈팅스타 원가 변경\t3.모두 변경]");
+			int select2 = sc.nextInt();
+			if(select2 == 1 ) {
+				System.out.println("변경할 원가를 입력하세요");
+				int changeMintExpense = sc.nextInt();
+				for(int i=0; i<monthSales.size(); i++) {
+					monthSales.get(i).setMintExpense(changeMintExpense);
+				}
+			} else if(select2 ==2) {
+				System.out.println("변경할 원가를 입력하세요");
+				int changeShootingExpense = sc.nextInt();
+				for(int i=0; i<monthSales.size(); i++) {
+					monthSales.get(i).setShootingStarExpense(changeShootingExpense);
+				}
+			} else if(select2 == 3) {
+				System.out.println("변경할 민트와 슈팅스타의 원가를 순서대로 입력하세요");
+				int changeMintExpense = sc.nextInt();
+				int changeShootingExpense = sc.nextInt();
+				for(int i=0; i<monthSales.size(); i++) {
+					monthSales.get(i).setMintExpense(changeMintExpense);
+					monthSales.get(i).setShootingStarExpense(changeShootingExpense);
+				}
+			}
+		}else {
+			System.out.println("다시 입력하세요");
+			changeExpense();
 		}
+	}
+	
+	public void exit() {
+		System.out.println("프로그램을 종료합니다");
+		sc.close();
+		System.exit(0);
 	}
 	
 	public void run() {
@@ -123,17 +191,12 @@ public class RunProgram {
 			switch(choice) {
 			case 1 : showSales(); break;
 			case 2 : inputSales(); break;
-			case 3 : modifySales(); break;
-			case 4 : compareSales(); break;
+			case 3 : compareSales(); break;
+			case 4 : changeExpense(); break;
 			case 0 : exit(); break;
 			default : System.out.println("다시 선택하세요");
 			}
 		}
 	}
 	
-	public void exit() {
-		System.out.println("프로그램을 종료합니다");
-		sc.close();
-		System.exit(0);
-	}
 }
--- MiniProject_IceCream/src/org/min/MiniP/SetProgram.java
+++ MiniProject_IceCream/src/org/min/MiniP/SetProgram.java
@@ -1,16 +1,21 @@
 package org.min.MiniP;
 
 public class SetProgram {
-
+	// 초기 판매가/ 원가
 	// 민트 판매가 : @3000원      슈팅스타 판매가 : @2500원
 	// 민트 원가 : @1000원        슈팅스타 원가 : @700원
 	
-	private int mint , shootingStar;
+	private int mint , shootingStar , mintPrice, shootingStarPrice, mintExpense, shootingStarExpense;
 	private double expense, sales, income;
 	
+	public SetProgram() {};
 	public SetProgram(int mint, int shootingStar) {
 		this.mint = mint;
 		this.shootingStar = shootingStar;
+		setMintPrice(3000);
+		setMintExpense(1000);
+		setShootingStarPrice(2500);
+		setShootingStarExpense(700);
 	}
 
 	public int getMintSalesVolume() {
@@ -22,15 +27,48 @@ public class SetProgram {
 	}
 	
 	public double getExpense() {
-		return expense = (mint*1000)+(shootingStar*700);
+		return expense = (double)(mint*mintExpense)+(shootingStar*shootingStarExpense) ;
 	}
 
 	public double getSales() {
-		return sales = (mint*3000)+(shootingStar*2500);
+		return sales = (double)(mint*mintPrice)+(shootingStar*shootingStarPrice);
 	}
 
 	public double getIncome() {
-		return income = (mint*2000)+(shootingStar*1800);
+		return income = (double)(mint*(mintPrice-mintExpense))+(shootingStar*(shootingStarPrice-shootingStarExpense));
 	}
 
+	public int getMintPrice() {
+		return mintPrice;
+	}
+
+	public int getShootingStarPrice() {
+		return shootingStarPrice;
+	}
+
+	public int getMintExpense() {
+		return mintExpense;
+	}
+
+	public int getShootingStarExpense() {
+		return shootingStarExpense;
+	}
+
+	public void setMintPrice(int mintPrice) {
+		this.mintPrice = mintPrice;
+	}
+
+	public void setShootingStarPrice(int shootingStarPrice) {
+		this.shootingStarPrice = shootingStarPrice;
+	}
+
+	public void setMintExpense(int mintExpense) {
+		this.mintExpense = mintExpense;
+	}
+
+	public void setShootingStarExpense(int shootingStarExpense) {
+		this.shootingStarExpense = shootingStarExpense;
+	}
+
+	
 }
